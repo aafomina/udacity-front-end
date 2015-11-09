030e3b2e1147149238273a030112bc9b17bf7e9d
Angelina Fomina <angelina.fomina@gmail.com>
2015-11-09 01:32:49 -0500

    add append and prepend code from lesson onw

---
+++ project-two/js/resumeBuilder.js
@@ -0,0 +1,7 @@
+var name = "Angelina Fomina";
+var role = "Entreprenur";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
